16c56f08c91144b03c83cc11d8841278e04bfbdd
Rajkarn Singh <rajkarn88@gmail.com>
2017-06-25 16:54:48 +0100

    Update printFunction.py
    Added comment, used range function

--- printFunction.py
+++ printFunction.py
@@ -1,4 +1,5 @@
+#Given N, print a string 123...N without spaces
 from __future__ import print_function
 if __name__ == '__main__':
     n = int(raw_input())
-    print(*(1,n+1), sep='')
+    print(*range(1,n+1), sep='')
